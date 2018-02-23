2834e4d59de038923851d3d3894a2767645d358d
davsobo <aureliousvayne57@gmail.com>
2018-02-23 08:49:13 +0700

    minor touchup to user level access

--- app/src/main/java/com/orpheus/rebuffer/ToolsActivity.java
+++ app/src/main/java/com/orpheus/rebuffer/ToolsActivity.java
@@ -2,12 +2,16 @@ package com.orpheus.rebuffer;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class ToolsActivity extends AppCompatActivity {
 
+    private TextView uname;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_tools);
+        uname= (TextView) findViewById(R.id.usernama);
+        uname.setText(LoginActivity.mEmail);
     }
 }
