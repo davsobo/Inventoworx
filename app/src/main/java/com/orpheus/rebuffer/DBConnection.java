36fd8a34ac6ade192a6b5cfe4ed26323f9edfa77
AttreaVe <jhnoa@users.noreply.github.com>
2018-02-26 11:01:46 +0700

    [Update] DB CONNECTION TO MY-PLACE.US FREE HOSTING
    invento.html-5.me

--- app/src/main/java/com/orpheus/rebuffer/DBConnection.java
+++ app/src/main/java/com/orpheus/rebuffer/DBConnection.java
@@ -6,7 +6,7 @@ package com.orpheus.rebuffer;
 
 public class DBConnection {
     //URL to our login.php file, url bisa diganti sesuai dengan alamat server kita
-    public static final String LOGIN_URL = "http://192.168.43.118/Inventoworx/login.php";
+    public static final String LOGIN_URL = "http://invento.html-5.me/login.php";
 
     //If server response is equal to this that means login is successful
     public static final String LOGIN_SUCCESS = "success";
