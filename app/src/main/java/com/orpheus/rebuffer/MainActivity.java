630bf21b58f2c3f825e453795a332ad37b4f73fe
AttreaVe <jhnoa@users.noreply.github.com>
2018-02-28 18:52:36 +0700

    [update] blom fix masih crash terus

--- app/src/main/java/com/orpheus/rebuffer/MainActivity.java
+++ app/src/main/java/com/orpheus/rebuffer/MainActivity.java
@@ -1,9 +1,14 @@
 package com.orpheus.rebuffer;
 
 import android.content.Intent;
+import android.os.Build;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
+import android.webkit.CookieManager;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -11,6 +16,14 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        WebView myWebView = (WebView) findViewById(R.id.webview);
+        myWebView.loadUrl("http://invento.html-5.me/");
+        WebSettings webSettings = myWebView.getSettings();
+        webSettings.setJavaScriptEnabled(true);
+
+        CookieManager.getInstance().setAcceptThirdPartyCookies(myWebView,true);
+        Log.d("JSON MAINACTIVITY", getCookie("http://invento.html-5.me/","__test"));
         UserData.fetchDataInventory(getApplicationContext());
     }
 
@@ -19,5 +32,21 @@ public class MainActivity extends AppCompatActivity {
         Intent intent = new Intent(MainActivity.this, LoginActivity.class);
         startActivity(intent);
         finish();
+    }
+
+    public String getCookie(String siteName,String CookieName){
+        String CookieValue = null;
+
+        CookieManager cookieManager = CookieManager.getInstance();
+        String cookies = cookieManager.getCookie(siteName);
+        String[] temp=cookies.split(";");
+        for (String ar1 : temp ){
+            if(ar1.contains(CookieName)){
+                String[] temp1=ar1.split("=");
+                CookieValue = temp1[1];
+                break;
+            }
+        }
+        return CookieValue;
     }
 }
