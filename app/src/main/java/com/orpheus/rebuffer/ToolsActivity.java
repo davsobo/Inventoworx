553ecff086b658bc804488cb28444671597cac31
davsobo <aureliousvayne57@gmail.com>
2018-03-01 01:55:50 +0700

    populated the spinner
    cookienya kyknya mood2an, kadang mau masuk kadang kgk

--- app/src/main/java/com/orpheus/rebuffer/ToolsActivity.java
+++ app/src/main/java/com/orpheus/rebuffer/ToolsActivity.java
@@ -65,7 +65,7 @@ public class ToolsActivity extends AppCompatActivity {
         new Handler().postDelayed(new Runnable() {
             @Override
             public void run() {
-                /**-------ini jatahnya spinner1-------**/
+                                    /**-------ini jatahnya spinner1-------**/
                 //Do something after 2s
                 Log.d("RMAP Entry", "onCreate: IM HERE");
                 String[] spinnerArray1 = new String[UserData.mapMerk.size()];
@@ -84,7 +84,7 @@ public class ToolsActivity extends AppCompatActivity {
                 spinner1.setAdapter(spinnerAdapter1);
                 Log.d("RMAP ENTRY", "run: ME HERE AFTER ADAPTER");
 
-                /**------ini jatahnya spinner 2-------**/
+                                    /**------ini jatahnya spinner 2-------**/
                 Log.d("RMAP Entry", "onCreate: IM HERE");
                 String[] spinnerArray2 = new String[UserData.mapTipe.size()];
                 Log.d("RMAP Entry", "onCreate: IM HERE BEFORE THE LOOP");
@@ -101,7 +101,79 @@ public class ToolsActivity extends AppCompatActivity {
                 spinnerAdapter2.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
                 spinner2.setAdapter(spinnerAdapter2);
                 Log.d("RMAP ENTRY", "run: ME HERE AFTER ADAPTER");
+
+                                    /**------ini jatahnya spinner3------**/
+                Log.d("RMAP Entry", "onCreate: IM HERE");
+                String[] spinnerArray3 = new String[UserData.mapUkuran.size()];
+                Log.d("RMAP Entry", "onCreate: IM HERE BEFORE THE LOOP");
+                HashMap<Integer,String> spinnerMap3 = new HashMap<Integer, String>();
+                Log.d("RMAP Entry", "run: "+UserData.mapUkuran.size());
+                for (int i = 0; i < UserData.mapUkuran.size(); i++)
+                {
+                    spinnerMap3.put(i,UserData.mapUkuran.get(i));
+                    spinnerArray3[i] = UserData.mapUkuran.get(i);
+                    Log.d("RMAP Entry", "onCreate: IM HERE IN THE LOOP");
+                }
+                Log.d("RMAP Entry", "onCreate: IM HERE AFTER THE LOOP");
+                ArrayAdapter<String> spinnerAdapter3 = new ArrayAdapter<String>(getApplicationContext(), android.R.layout.simple_spinner_item, spinnerArray3);
+                spinnerAdapter3.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+                spinner3.setAdapter(spinnerAdapter3);
+                Log.d("RMAP ENTRY", "run: ME HERE AFTER ADAPTER");
+
+                                    /**------ini jatahnya spinner4------**/
+                Log.d("RMAP Entry", "onCreate: IM HERE");
+                String[] spinnerArray4 = new String[UserData.mapBahan.size()];
+                Log.d("RMAP Entry", "onCreate: IM HERE BEFORE THE LOOP");
+                HashMap<Integer,String> spinnerMap4 = new HashMap<Integer, String>();
+                Log.d("RMAP Entry", "run: "+UserData.mapBahan.size());
+                for (int i = 0; i < UserData.mapBahan.size(); i++)
+                {
+                    spinnerMap4.put(i,UserData.mapBahan.get(i));
+                    spinnerArray4[i] = UserData.mapBahan.get(i);
+                    Log.d("RMAP Entry", "onCreate: IM HERE IN THE LOOP");
+                }
+                Log.d("RMAP Entry", "onCreate: IM HERE AFTER THE LOOP");
+                ArrayAdapter<String> spinnerAdapter4 = new ArrayAdapter<String>(getApplicationContext(), android.R.layout.simple_spinner_item, spinnerArray4);
+                spinnerAdapter4.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+                spinner4.setAdapter(spinnerAdapter4);
+                Log.d("RMAP ENTRY", "run: ME HERE AFTER ADAPTER");
+
+                                    /**------ini jatahnya spinner5------**/
+                Log.d("RMAP Entry", "onCreate: IM HERE");
+                String[] spinnerArray5 = new String[UserData.mapJumlah.size()];
+                Log.d("RMAP Entry", "onCreate: IM HERE BEFORE THE LOOP");
+                HashMap<Integer,String> spinnerMap5 = new HashMap<Integer, String>();
+                Log.d("RMAP Entry", "run: "+UserData.mapJumlah.size());
+                for (int i = 0; i < UserData.mapJumlah.size(); i++)
+                {
+                    spinnerMap5.put(i,UserData.mapJumlah.get(i));
+                    spinnerArray5[i] = UserData.mapJumlah.get(i);
+                    Log.d("RMAP Entry", "onCreate: IM HERE IN THE LOOP");
+                }
+                Log.d("RMAP Entry", "onCreate: IM HERE AFTER THE LOOP");
+                ArrayAdapter<String> spinnerAdapter5 = new ArrayAdapter<String>(getApplicationContext(), android.R.layout.simple_spinner_item, spinnerArray5);
+                spinnerAdapter5.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+                spinner5.setAdapter(spinnerAdapter5);
+                Log.d("RMAP ENTRY", "run: ME HERE AFTER ADAPTER");
+
+                                    /**------ini jatahnya spinner6------**/
+                Log.d("RMAP Entry", "onCreate: IM HERE");
+                String[] spinnerArray6 = new String[UserData.mapLokasi.size()];
+                Log.d("RMAP Entry", "onCreate: IM HERE BEFORE THE LOOP");
+                HashMap<Integer,String> spinnerMap6 = new HashMap<Integer, String>();
+                Log.d("RMAP Entry", "run: "+UserData.mapLokasi.size());
+                for (int i = 0; i < UserData.mapLokasi.size(); i++)
+                {
+                    spinnerMap6.put(i,UserData.mapLokasi.get(i));
+                    spinnerArray6[i] = UserData.mapLokasi.get(i);
+                    Log.d("RMAP Entry", "onCreate: IM HERE IN THE LOOP");
+                }
+                Log.d("RMAP Entry", "onCreate: IM HERE AFTER THE LOOP");
+                ArrayAdapter<String> spinnerAdapter6 = new ArrayAdapter<String>(getApplicationContext(), android.R.layout.simple_spinner_item, spinnerArray6);
+                spinnerAdapter6.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+                spinner6.setAdapter(spinnerAdapter6);
+                Log.d("RMAP ENTRY", "run: ME HERE AFTER ADAPTER");
             }
-        }, 1000);
+        }, 3000);
     }
 }
