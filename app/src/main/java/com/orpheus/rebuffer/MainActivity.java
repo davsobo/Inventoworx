b5bf84a799c5a91b75adbd7032680c959f32b577
AttreaVe <jhnoa@users.noreply.github.com>
2018-02-28 17:10:49 +0700

    ok

--- app/src/main/java/com/orpheus/rebuffer/MainActivity.java
+++ app/src/main/java/com/orpheus/rebuffer/MainActivity.java
@@ -1,9 +1,13 @@
 package com.orpheus.rebuffer;
 
 import android.content.Intent;
+import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
+
+import java.util.Map;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -12,6 +16,23 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         UserData.fetchDataInventory(getApplicationContext());
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Log.d("JSON TeST", "I'M HERE");
+                Log.d("JSON TEST", UserData.mapInventory.toString());
+                for (Map.Entry<String, Map<String,String>> entry : UserData.mapInventory.entrySet())
+                {
+                    String key = entry.getKey();
+                    Log.d("Json Response: Main", "key = " + key);
+                    for(Map.Entry<String,String> entry1: entry.getValue().entrySet() )
+                    {
+                        Log.d("Json Response: Main", entry1.getValue());
+                    }
+                }
+            }
+        },2000);
+
     }
 
     //*****-----Ini buat ganti ke page berikutnya-----*****//
